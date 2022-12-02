aa44fafd494fac7b208e84a7d35411fb16afca2b
Wando Borges <wanbrwan@gmail.com>
2022-12-02 20:34:47 -0300

    Unique index

--- src/main/java/com/wtiinfo/model/Student.java
+++ src/main/java/com/wtiinfo/model/Student.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.util.List;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.Data;
 
@@ -14,9 +15,13 @@ import lombok.Data;
 public class Student {
    @Id
    private String id;
+
    private String firstName;
    private String lastName;
+
+   @Indexed(unique = true)
    private String email;
+
    private Gender gender;
    private Address address;
    private List<String> favouriteSubjects;
