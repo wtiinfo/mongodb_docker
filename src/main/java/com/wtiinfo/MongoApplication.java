29c144223b989cbcccebe6276d0143d79102bbb1
Wando Borges <wanbrwan@gmail.com>
2022-12-02 21:08:04 -0300

    Custom query

--- src/main/java/com/wtiinfo/MongoApplication.java
+++ src/main/java/com/wtiinfo/MongoApplication.java
@@ -30,21 +30,32 @@ public class MongoApplication {
 					Gender.MALE, new Address("Brasil", "São Paulo", "09400-400"),
 					List.of("Anatomia", "Criação de Games", "VFX"), BigDecimal.TEN, LocalDateTime.now()
 			);
+			//mongoTemplateAndQuery(repository, mongoTemplate, student);
 
-			Query query = new Query();
-			query.addCriteria(Criteria.where("email").is(student.getEmail()));
-			List<Student> students = mongoTemplate.find(query, Student.class);
-
-			if(students.size() > 1) {
-				throw new IllegalStateException("Found students with this email " + student.getEmail());
-			}
-			if(students.isEmpty()) {
-				System.out.println("LOG: Insert student " + student);
+			repository.findStudentByEmail(student.getEmail()).ifPresentOrElse(s -> {
+				System.out.println(s + " already exists");
+			}, () -> {
+				System.out.println("Inserting student " + student);
 				repository.insert(student);
-			} else {
-				System.out.println("LOG: Email in use");
-			}
+			});
+
 		};
+	}
+
+	private void mongoTemplateAndQuery(StudentRepository repository, MongoTemplate mongoTemplate, Student student) {
+		Query query = new Query();
+		query.addCriteria(Criteria.where("email").is(student.getEmail()));
+		List<Student> students = mongoTemplate.find(query, Student.class);
+
+		if(students.size() > 1) {
+			throw new IllegalStateException("Found students with this email " + student.getEmail());
+		}
+		if(students.isEmpty()) {
+			System.out.println("LOG: Inserting student " + student);
+			repository.insert(student);
+		} else {
+			System.out.println("LOG: " + student  + " already exists");
+		}
 	}
 
 }
--- src/main/java/com/wtiinfo/repository/StudentRepository.java
+++ src/main/java/com/wtiinfo/repository/StudentRepository.java
@@ -1,9 +1,11 @@
 package com.wtiinfo.repository;
 
 import com.wtiinfo.model.Student;
+import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository extends MongoRepository<Student, String> {
+    Optional<Student> findStudentByEmail(String email);
 }
